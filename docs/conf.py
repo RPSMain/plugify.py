bcdece1e4ad233086a29784ac04f19fa84212d23
Vincent <vincentbusiness55@gmail.com>
2021-12-18 18:52:42 +0800

    wrong version

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = "2021, RPS"
 author = "RPS"
 
 # The full version, including alpha/beta/rc tags
-release = "0.2.0"
+release = "0.1.0"
 
 
 # -- General configuration ---------------------------------------------------
